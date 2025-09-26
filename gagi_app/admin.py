31007e6750d4e76f2d3f823fa3bf70f94803e775
Студент группы IS <is@sielom.local>
2025-09-26 09:41:39 +0500

    26.09

--- gagi_app/admin.py
+++ gagi_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import *
-admin.site.is_registered(Author)
-admin.site.is_registered(Book)
-admin.site.is_registered(PUblisher)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(PUblisher)
--- gagi_app/models.py
+++ gagi_app/models.py
@@ -2,9 +2,26 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(verbose_name='Имя автора', max_length=20)
-    surnabe = models.CharField('Фамилия', max_length=25)
+    surname = models.CharField('Фамилия', max_length=25)
     birthday = models.DateField('Дата рождения')
     bio = models.TextField('Биография')
+    desc = models.CharField("Умер или нет", default="No")
+
+    class Meta: 
+        verbose_name = "Автор"
+        verbose_name_plural = "Автор"
+        ordering = ["surname", "name"]
+        indexes = [
+            models.Index(fields=["surname"])
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields = ["surname", "bio"],
+                condition = models.Q(desc = "Жив"),
+                name = "unique_surname_bio"
+            ),
+            
+        ]
 
 class PUblisher(models.Model):
     name = models.CharField('НАзвание', unique=True)
@@ -13,3 +30,6 @@ class Book(models.Model):
     title = models.CharField('Название', max_length=50)
     id_publisher = models.ForeignKey(PUblisher, on_delete=models.CASCADE)
     id_autor = models.ManyToManyField(Author)
+
+def __str__(self):
+    return f"{self.surname}{self.name}"
