e1eedd49afbc24041ea918a29106cf687e0c3b30
Minmin77-lab <panahova52@gmail.com>
2025-09-26 17:03:47 +0500

    на всякий случай(из-за отката)

--- gagi_app/admin.py
+++ gagi_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Users)
-# admin.site.register(Book)
-# admin.site.register(PUblisher)
+admin.site.register(Users)  
+admin.site.register(Staff)
+admin.site.register(Attractions)
+admin.site.register(TicketTypes)  
+# admin.site.register(Tickets)
--- gagi_app/models.py
+++ gagi_app/models.py
@@ -5,7 +5,7 @@ class Users(models.Model):
     surname = models.CharField('Фамилия', max_length=25)
     birthday = models.DateField('Дата рождения')
     phone_number = models.CharField('Номер телефона', max_length=20)
-    email = models.CharField('e-mail', max_length=100)
+    email = models.CharField('e-mail', max_length=100, unique=True)
     hash_password = models.CharField('Пароль', max_length=255)
     created_at = models.DateTimeField('Дата и время регистрации')
     def __str__(self):
@@ -23,8 +23,79 @@ class Users(models.Model):
                 fields = ["surname", "email"],
                 name = "unique_surname_email"
             ),
-            
         ]
+
+class Staff(models.Model):
+    # сделать tip для должности
+    name = models.CharField('Имя', max_length=50)
+    surname = models.CharField('Фамилия', max_length=50)
+    position = models.CharField('Должность', max_length=50)
+    phone_number = models.CharField('Номер телефона', max_length=20)
+    passport = models.CharField('Паспорт', max_length=13, unique=True)
+    
+    def __str__(self):
+        return f"{self.name} {self.surname} {self.position}"
+    
+    class Meta:
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
+        ordering = ["name", "surname", "position"]
+        indexes = [
+            models.Index(fields=["position"])
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields = ["surname", "passport"],
+                name = "unique_surname_passport"
+            ),
+        ]
+
+class Attractions(models.Model):
+    # сделать tip для роста, возраста, вместимости и продолжительности
+    name = models.CharField('Название', max_length=100, unique=True)
+    min_height = models.IntegerField('Минимальный рост')
+    max_height = models.IntegerField('Максимальный рост')
+    min_age = models.IntegerField('Минимальный возраст')
+    activity_status = models.BooleanField('Статус активности', default=True)
+    capacity = models.IntegerField('Вместимость')
+    duration_seconds = models.IntegerField('Продолжительность (сек)')
+    staff = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Ответственный сотрудник')
+    
+    def __str__(self):
+        return f"{self.name} {self.staff}"
+    
+    class Meta:
+        verbose_name = "Аттракцион"
+        verbose_name_plural = "Аттракционы"
+        ordering = ["name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields = ["name"],
+                name = "unique_name_of_attraction"
+            ),
+        ]
+
+class TicketTypes(models.Model):
+    # сделать tip для цены, названия, срока действия
+    name = models.CharField('Название', max_length=50, unique=True)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    validity_duration = models.IntegerField('Срок действия (дни)')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Тип билета"
+        verbose_name_plural = "Типы билетов"
+        ordering = ["name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields = ["name"],
+                name = "unique_name_of_type"
+            ),
+        ]
+
+
 
 
 # class PUblisher(models.Model):
