f9cbd660c486562369e5299e996eace881084456
Студент группы IS <is@sielom.local>
2025-09-26 11:20:55 +0500

    начало уп

--- gagi_app/admin.py
+++ gagi_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(PUblisher)
+admin.site.register(Users)
+# admin.site.register(Book)
+# admin.site.register(PUblisher)
--- gagi_app/models.py
+++ gagi_app/models.py
@@ -1,36 +1,37 @@
 from django.db import models
 
-class Author(models.Model):
-    name = models.CharField(verbose_name='Имя автора', max_length=20)
+class Users(models.Model):
+    name = models.CharField(verbose_name='Имя', max_length=20)
     surname = models.CharField('Фамилия', max_length=25)
     birthday = models.DateField('Дата рождения')
-    bio = models.TextField('Биография')
-    desc = models.CharField("Умер или нет", default="No")
+    phone_number = models.CharField('Номер телефона', max_length=20)
+    email = models.CharField('e-mail', max_length=100)
+    hash_password = models.CharField('Пароль', max_length=255)
+    created_at = models.DateTimeField('Дата и время регистрации')
     def __str__(self):
         return f"{self.surname}{self.name}"
 
     class Meta: 
-        verbose_name = "Автор"
-        verbose_name_plural = "Автор"
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
         ordering = ["surname", "name"]
         indexes = [
             models.Index(fields=["surname"])
         ]
         constraints = [
             models.UniqueConstraint(
-                fields = ["surname", "bio"],
-                condition = models.Q(desc = "Жив"),
-                name = "unique_surname_bio"
+                fields = ["surname", "email"],
+                name = "unique_surname_email"
             ),
             
         ]
 
 
-class PUblisher(models.Model):
-    name = models.CharField('НАзвание', unique=True)
+# class PUblisher(models.Model):
+#     name = models.CharField('НАзвание', unique=True)
 
-class Book(models.Model):
-    title = models.CharField('Название', max_length=50)
-    id_publisher = models.ForeignKey(PUblisher, on_delete=models.CASCADE)
-    id_autor = models.ManyToManyField(Author)
+# class Book(models.Model):
+#     title = models.CharField('Название', max_length=50)
+#     id_publisher = models.ForeignKey(PUblisher, on_delete=models.CASCADE)
+#     id_autor = models.ManyToManyField(Author)
 
